86a0962afad960890f774456105283e17222e680
MusanjeLawrence <musanjelawrence6@gmail.com]>
2024-10-17 17:29:15 +0300

    integrating the corona admin dashboard template e

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,4 +1,19 @@
-<x-app-layout>
-  
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+      @include('admin.sidebar')
 
-</x-app-layout>
+     @include('admin.header')
+        <!-- partial -->
+
+        @include('admin.body')
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script')
+  </body>
+</html>
